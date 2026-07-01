45f98c95083d5747f0d1d0680bced7e59e918bce
Esing-botShop <rhyny9snhb@privaterelay.appleid.com>
2026-07-02 02:38:15 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ from aiohttp import web
 # === CONFIGURATION (from environment variables) ===
 API_ID = int(os.environ.get('API_ID', 0))
 API_HASH = os.environ.get('API_HASH', '')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+BOT_TOKEN = os.environ.get('8893741696:AAGtWe-XgCF0me3wmYgVWnM9v9_y2mF86FE', '')
 ADMIN_ID = int(os.environ.get('ADMIN_ID', 0))
 CHANNEL_LINK = os.environ.get('CHANNEL_LINK', 'https://t.me/perexodnikTIOMKI')
 PORT = int(os.environ.get('PORT', 8080))
